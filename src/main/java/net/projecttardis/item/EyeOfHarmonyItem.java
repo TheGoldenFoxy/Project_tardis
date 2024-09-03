77d01c60924f253a9fffddb87018a6e61afd8771
TheGoldenFoxy <Damie@DESKTOP-RKDPLD9.broadband>
2024-09-03 23:25:45 +0100

    basic items added, dev textures used

---
+++ src/main/java/net/projecttardis/item/EyeOfHarmonyItem.java
@@ -0,0 +1,44 @@
+
+package net.projecttardis.item;
+
+import net.projecttardis.ProjectTardisModElements;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+@ProjectTardisModElements.ModElement.Tag
+public class EyeOfHarmonyItem extends ProjectTardisModElements.ModElement {
+	@ObjectHolder("project_tardis:eye_of_harmony")
+	public static final Item block = null;
+
+	public EyeOfHarmonyItem(ProjectTardisModElements instance) {
+		super(instance, 6);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("eye_of_harmony");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/net/projecttardis/item/TardisShellItem.java
@@ -0,0 +1,44 @@
+
+package net.projecttardis.item;
+
+import net.projecttardis.ProjectTardisModElements;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+@ProjectTardisModElements.ModElement.Tag
+public class TardisShellItem extends ProjectTardisModElements.ModElement {
+	@ObjectHolder("project_tardis:tardis_shell")
+	public static final Item block = null;
+
+	public TardisShellItem(ProjectTardisModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("tardis_shell");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
