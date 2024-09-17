f566e4cac84ff7f3e4b80c0ada77a0f7eb7503ae
Taygamer <tidge@DESKTOP-TAY.home>
2024-09-17 16:10:10 +0100

    Fixing some things

--- src/main/java/net/projecttardis/item/EyeOfHarmonyItem.java
+++ src/main/java/net/projecttardis/item/EyeOfHarmonyItem.java
@@ -8,6 +8,6 @@ import net.minecraft.item.Item;
 
 public class EyeOfHarmonyItem extends Item {
 	public EyeOfHarmonyItem() {
-		super(new Item.Properties().group(ProjectTardisModTabs.TAB_PROJECT_TARDIS).maxStackSize(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().group(ProjectTardisModTabs.TAB_PROJECT_TARDIS).maxStackSize(1).rarity(Rarity.COMMON));
 	}
 }
