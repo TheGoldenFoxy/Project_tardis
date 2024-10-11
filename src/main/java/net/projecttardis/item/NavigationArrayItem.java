6430f53bad404f7f46877a100793d3571f0164c5
Taygamer <tidge@Apex-1.home>
2024-10-11 20:24:41 +0100

    Added Nav array, Added Suncore

--- src/main/java/net/projecttardis/item/NavigationArrayItem.java
+++ src/main/java/net/projecttardis/item/NavigationArrayItem.java
@@ -1,12 +1,13 @@
 
 package net.projecttardis.item;
 
+import net.projecttardis.init.ProjectTardisModTabs;
+
 import net.minecraft.item.Rarity;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 
 public class NavigationArrayItem extends Item {
 	public NavigationArrayItem() {
-		super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().group(ProjectTardisModTabs.TAB_PROJECT_TARDIS).maxStackSize(1).rarity(Rarity.COMMON));
 	}
 }
